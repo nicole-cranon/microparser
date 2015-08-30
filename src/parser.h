6136207051ac743d1f41a78dded8e4eda5f34cf5
Nicole Cranon <nicole.cranon@gmail.com>
2015-08-29 23:11:44 -0600

    added main and parser header file

---
+++ src/parser.h
@@ -0,0 +1,15 @@
+/*
+  nicole cranon <nicole.cranon@ucdenver.edu>
+  csci 4640, fall 2015
+  assignment 2 - micro parser
+ */
+
+#ifndef _PARSER
+#define _PARSER
+
+#include "microscanner/src/scanner.h"
+
+
+
+
+#endif
